36d25bed9ef94480457413fb42c5b69ede82220c
den <d.n.voropaev@gmail.com>
2020-07-16 22:02:15 +0300

    template swift ui project

---
+++ MarketCap/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MarketCap
+//
+//  Created by Denis on 16.07.2020.
+//  Copyright © 2020 Denis Voropaev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
